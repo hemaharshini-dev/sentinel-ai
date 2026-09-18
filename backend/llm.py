01923d4056df85fef50f505d24d294109ae3b779
Hema Harshini Pydimarri <23wh1a0586@bvrithyderabad.edu.in>
2026-09-18 21:09:19 +0530

    Added temperature, example env file

--- backend/llm.py
+++ backend/llm.py
@@ -6,6 +6,7 @@ from langchain_groq import ChatGroq
 load_dotenv()
 
 llm = ChatGroq(
-    model="llama-3.3-70b-versatile",
+    model="openai/gpt-oss-20b",
     api_key=os.getenv("GROQ_API_KEY"),
+    temperature=0,
 )
